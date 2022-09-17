e404f3146ab6886ee0464f93b6a0135d207a735f
Jean Pierre <jeanpierre.webedev@gmail.com>
2022-09-17 07:33:53 -0500

    error and 404 pages added

---
+++ pages/_error.js
@@ -0,0 +1,16 @@
+import { getGenerals } from 'lib/getGenerals'
+
+export default function Custom404() {
+  return <h1>500 - Page Not Found</h1>
+}
+
+export const getStaticProps = async () => {
+  const { options, polylang } = await getGenerals()
+
+  return {
+    props: {
+      options,
+      polylang
+    }
+  }
+}
